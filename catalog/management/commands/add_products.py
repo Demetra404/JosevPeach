986063077207893ea4af72d34fe5da474028ee11
demetra <jennyani98@gmail.com>
2026-01-15 15:56:09 +0300

    fixed error with names key in add_products.py

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -7,8 +7,8 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         category, _ = Category.objects.get_or_create(name_category='Оперативная память', description_category='драгоценность')
         products = [
-            {'name_products':'Pioneer', 'description_product':'viper venom ddr5', 'category_product':category, 'price_product':1000000},
-            {'name_products':'Kingston', 'description_product':'перепаянная и разогнанная ddr4', 'category_product':category, 'price_product':10000},
+            {'name_product':'Pioneer', 'description_product':'viper venom ddr5', 'category_product':category, 'price_product':1000000},
+            {'name_product':'Kingston', 'description_product':'перепаянная и разогнанная ddr4', 'category_product':category, 'price_product':10000},
         ]
         for product in products:
             product, created = Product.objects.get_or_create(**product)
