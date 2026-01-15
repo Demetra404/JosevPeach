2ae65d1366083807f753487758703177e192d572
demetra <jennyani98@gmail.com>
2026-01-15 15:36:11 +0300

    add custom command

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -4,3 +4,15 @@ from catalog.models import Product, Category
 class Command(BaseCommand):
     help = 'Add test products'
 
+    def handle(self, *args, **kwargs):
+        category, _ = Category.objects.get_or_create(name_category='Оперативная память', description_category='драгоценность')
+        products = [
+            {'name_products':'Pioneer', 'description_product':'viper venom ddr5', 'category_product':category, 'price_product':1000000},
+            {'name_products':'Kingston', 'description_product':'перепаянная и разогнанная ddr4', 'category_product':category, 'price_product':10000},
+        ]
+        for product in products:
+            product, created = Product.objects.get_or_create(**product)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'yes {product.name_product}'))
+            else:
+                self.stdout.write(self.style.WARNING(f'no {product.name_product}'))
