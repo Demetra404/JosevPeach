1d57539970764e26708ca8daa543be66f1e06ced
demetra <jennyani98@gmail.com>
2026-01-20 15:55:57 +0300

    modify app catalog with help classes

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
-from .models import Product
+from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView
+
+from .models import Product, Category
 
 # Create your views here.
-def home_view(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'catalog/home.html', context=context)
-def contacts_view(request):
-    return render(request, 'catalog/contacts.html')
-def product_info(request, product_id):
-    product = Product.objects.get(id=product_id)
-    context = {'product': product}
-    return render(request, 'catalog/product_info.html', context=context)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_info.html'
+    context_object_name = 'product'
+class HomeListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'home'
+
+class ContactsListView(ListView):
+    model = Category # как заглушка
+    template_name = 'catalog/contacts.html'
+    context_object_name = 'contacts'
+#def contacts_view(request):
+    #return render(request, 'catalog/contacts.html')
