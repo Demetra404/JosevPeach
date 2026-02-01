6ffea2505c4015f940ee9f8dcb61bb580c88c541
demetra <jennyani98@gmail.com>
2026-02-01 17:24:25 +0300

    fixed work whith rules access and bugfix

--- catalog/views.py
+++ catalog/views.py
@@ -30,12 +30,26 @@ class ProductUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView)
     success_url = reverse_lazy('catalog:products')
     permission_required = 'catalog.change_product'
 
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.owner != request.user:
+            return HttpResponseForbidden("Вы не владелец продукта")
+        return super().dispatch(request, *args, **kwargs)
 
-class ProductDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_delete.html'
     success_url = reverse_lazy('catalog:products')
-    permission_required = 'catalog.delete_product'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        is_owner = obj.owner == request.user
+        is_moderator = request.user.has_perm('catalog.delete_product')
+
+        if not (is_owner or is_moderator):
+            return HttpResponseForbidden("Вы не можете удалить продукт")
+        return super().dispatch(request, *args, **kwargs)
 
 class ProductDetailView(DetailView):
     model = Product
@@ -59,7 +73,7 @@ class UnpublishProductView(LoginRequiredMixin, View):
         if not request.user.has_perm('catalog.can_unpublish_product'):
             return HttpResponseForbidden("У вас нет прав редактирования")
 
-        product.publication_status = True
+        product.publication_status = False
         product.save()
 
         return redirect('catalog:product_detail', pk=pk)
