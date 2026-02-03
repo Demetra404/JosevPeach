eea24b0f912364e5d22c5b5dbb9b1d436bea8aa7
demetra <jennyani98@gmail.com>
2026-02-03 15:21:57 +0300

    add modul services with class for compare product category

---
+++ catalog/services.py
@@ -0,0 +1,21 @@
+from .models import Product, Category
+
+class ProductServiceNotWork:
+
+    @staticmethod
+    def products_in_category(category):
+        products = Product.objects.filter(category_product = category)
+        list_products = []
+        #if not product.exist():
+            #return None
+        products_in = list_products.append(product.name_product for product in products)
+        return products_in
+
+class ProductService:
+
+    @staticmethod
+    def get_products_in_category(category_id):
+        products_in_category = Product.objects.filter(category_product_id=category_id)
+        if not products_in_category.exists():
+            return None
+        return products_in_category
